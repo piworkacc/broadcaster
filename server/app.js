f9fb30a089dd13a551933d6df3c1135f011e1e45
Ilya <toupitsyn@gmail.com>
2022-03-17 16:43:55 +0300

    useless tuning

--- server/app.js
+++ server/app.js
@@ -6,24 +6,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const nms = require('./mediaServer');
 
-const NodeMediaServer = require('node-media-server');
-
-const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60,
-  },
-  http: {
-    port: 8000,
-    allow_origin: '*',
-  },
-};
-
-const nms = new NodeMediaServer(config);
 nms.run();
 
 const indexRouter = require('./routes/index');
--- server/config/default.js
+++ server/config/default.js
@@ -2,30 +2,42 @@ const config = {
   server: {
     secret: 'kjVkuti2xAyF3JGCzSZTk0YWM5JhI9mgQW4rytXc',
   },
+  logType: 3,
   rtmp_server: {
     rtmp: {
       port: 1935,
-      chunk_size: 60000,
-      gop_cache: true,
+      chunk_size: 500,
+      gop_cache: false,
       ping: 60,
       ping_timeout: 30,
     },
     http: {
-      port: 8888,
+      port: 8000,
       mediaroot: './server/media',
       allow_origin: '*',
     },
     trans: {
       ffmpeg: '/usr/bin/ffmpeg',
-      tasks: [
-        {
-          app: 'live',
-          hls: true,
-          hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-          dash: true,
-          dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
-        },
-      ],
+      //   tasks: [
+      //     {
+      //       app: 'live',
+      //       vcParam: [
+      //         '-c:v',
+      //         'libx264',
+      //         '-vf',
+      //         '-b:v',
+      //         '2800k',
+      //         '-bufsize',
+      //         '4200k',
+      //         '-preset',
+      //         'fast',
+      //       ],
+      //       ac: 'aac',
+      //       acParam: ['-b:a', '128k', '-ar', 48000],
+      //       mp4: true,
+      //       mp4Flags: '[movflags=faststart]',
+      //     },
+      //   ],
     },
   },
 };
