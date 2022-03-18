d48270f5a46a03247493c55348d6b5a91c02061e
Ekaterina <ekaterina.gnedenko@gmail.com>
2022-03-18 15:45:33 +0300

    login форма в общем стиле

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Form, Input, Button } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { Form, Input, Button, Row, Col, Card } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import useUxios from '../../hooks/useUxios';
 import ErrorComponent from '../ErrorComponent/index';
 import Loading from '../Loading/index';
 import { loginAC } from '../../redux/sagas/sagasAC';
+import './Login.css';
+import { searchInput } from '../Search/Search'
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -34,60 +36,91 @@ const Login = () => {
   };
 
   return (
-    <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: 'Введите свой email!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <div className='container'>
+      <Row>
+        <Col span={12} offset={6}>
+          <Card 
+          className="login-form-card"
+          style={{ marginTop: '100px' }}
+          >
+            <Form
+              name="basic"
+              className="login-form"
+              wrapperCol={{
+                span: 24,
+              }}
+              initialValues={{
+                remember: true,
+              }}
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Введите пароль',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+              // style={{ width: '600px', marginTop: '100px' }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <div className='loginFormText'>
+                Войти в Veschatel:
+              </div>
+              <Form.Item
+                // label="Email"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Введите свой email!',
+                  },
+                ]}
+              >
+                <Input
+                  placeholder="email"
+                  style={searchInput}
+                />
+              </Form.Item>
 
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Войти
-        </Button>
-      </Form.Item>
-      <ErrorComponent message={error} />
-      <Loading loading={loading} />
-    </Form>
+              <Form.Item
+                // label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Введите пароль',
+                  },
+                ]}
+              >
+                <Input
+                  type="password"
+                  placeholder="пароль"
+                  style={searchInput}
+                />
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 8,
+                }}
+              >
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  Войти
+                </Button>
+                <div className='loginFormText'>
+                  Или <Link to="/signin">зарегестрируйтесь сейчас!</Link>
+                </div>
+
+              </Form.Item>
+              <div className='errorText'>
+                <ErrorComponent message={error} />
+                <Loading loading={loading} />
+              </div>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
