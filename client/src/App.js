b84dc4ae39ee40c01f12fa2cd29066ca388594d5
Ivan Puzyrev <puzirev.work@gmail.com>
2022-03-19 16:17:27 +0300

    Закоментил импорт

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Signup from './components/Signup/Signup';
 import Logout from './components/Logout/Logout';
 import Header from "./components/Header/Header.component";
 import UserProfile from './components/UserProfile/UserProfile';
-import Chat from './components/Chat';
+// import Chat from './components/Chat';
 
 const socket = io.connect('http://localhost:3002');
 
