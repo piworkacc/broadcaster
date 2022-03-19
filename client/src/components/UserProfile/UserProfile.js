9f59568c800962c34b051fce9d7f02b42e10ead0
Ekaterina <ekaterina.gnedenko@gmail.com>
2022-03-19 17:41:09 +0300

    userProfile auth protected

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -1,27 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout, Menu } from 'antd';
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import './UserProfile.css';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const { Header, Content, Footer, Sider } = Layout;
 
 const UserProfile = () => {
 
   const [selectedMenuItem, setSelectedMenuItem] = useState('item1');
+  const auth = useSelector((store) => store.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.ok) {
+      navigate('/login');
+    }
+  }, [auth, navigate]);
 
   const componentsSwitch = (key) => {
     switch (key) {
       case '1':
         return (
-        <h1>item1</h1>
+          <h1>item1</h1>
         );
       case '2':
         return (
-        <h1>item2</h1>
+          <h1>item2</h1>
         );
       case '3':
         return (
-        <h3>item3</h3>
+          <h3>item3</h3>
         );
       default:
         break;
@@ -41,21 +52,27 @@ const UserProfile = () => {
             console.log(collapsed, type);
           }}
         >
-          <div className="logo" />
+          <HelloUserName className="logo" >
+            <span>Привет, Username!</span>
+          </HelloUserName>
           <Menu
             theme="dark"
             mode="inline"
             selectedKeys={selectedMenuItem}
             onClick={(e) =>
               setSelectedMenuItem(e.key)}
-            >
+          >
             <Menu.Item
               key="1"
               icon={<UserOutlined />}
             >
-              nav 1
+              <span>Аккаунт</span>
+              {/* <Link to="settings" /> */}
             </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            <Menu.Item 
+            key="2"
+            icon={<VideoCameraOutlined/>}
+            >
               nav 2
             </Menu.Item>
             <Menu.Item key="3" icon={<UploadOutlined />}>
@@ -64,10 +81,12 @@ const UserProfile = () => {
           </Menu>
         </Sider>
         <Layout>
-          <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+          {/* <Header className="site-layout-sub-header-background" style={{ padding: 0 }} /> */}
           <Content style={{ margin: '24px 16px 0' }}>
             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              content
+              <DivContainer>
+                <StartStreamButton>Начать стрим</StartStreamButton>
+              </DivContainer>
               {componentsSwitch(selectedMenuItem)}
             </div>
           </Content>
@@ -79,3 +98,31 @@ const UserProfile = () => {
 }
 
 export default UserProfile;
+
+const HelloUserName = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`
+const StartStreamButton = styled.button`
+    font-family: 'Robert Sans Medium', Arial, sans-serif;
+    color: #fff;
+    margin-right: 30px;
+    width: 150px;
+    height: 40px;
+    background-color: #ee4540;
+    border-radius: 20px;
+    border: none;
+    transition: scale .4s ease;
+    &hover: {
+      transform:scale(1.1)
+    }
+`
+const DivContainer = styled.div`
+&:hover ${StartStreamButton} {
+    transform: scale(1.1);
+  }
+`
