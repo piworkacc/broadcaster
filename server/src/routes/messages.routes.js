ca39e3872466783b552f1de4c91a820ccf9acbe0
Ivan Puzyrev <puzirev.work@gmail.com>
2022-03-19 16:05:02 +0300

    Ручка для получения сообщений чата в разрезе стрима

---
+++ server/src/routes/messages.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const router = express.Router();
+const { Message } = require('../../db/models');
+
+/* GET home page. */
+router.get('/:stream_id', async (req, res, next) => {
+  try {
+    const chatMessages = await Message.findAll({
+      where: {
+        stream_id: req.params.stream_id,
+      },
+    });
+    res.json(chatMessages);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
