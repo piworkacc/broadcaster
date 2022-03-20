8ec5d022dca3debcc9aa7a81bb9ffc49ce21e935
Ivan Puzyrev <puzirev.work@gmail.com>
2022-03-20 18:30:49 +0300

    Прикрутил чат к базе

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import './styles.css'
 
-export default function Chat({ socket, stream_id = 1, user = 'Global' }) {
+export default function Chat({ socket, stream_id = 1, user = 1 }) {
 
-  const [chatMessages, setChatMessages] = useState([{}]);
+  const [chatMessages, setChatMessages] = useState([]);
 
   useEffect(() => {
 
     async function fetchData() {
       const response = await fetch(`/messages/${stream_id}`);
       const data = await response.json();
+      console.log(data);
       setChatMessages((prev) => [...prev, ...data])
     }
     fetchData();
@@ -18,9 +19,10 @@ export default function Chat({ socket, stream_id = 1, user = 'Global' }) {
 
   useEffect(() => {
 
-    socket.on('message:send', (msg) => {
-      console.log('Обработал message:send...', msg, user);
-      setChatMessages((prev) => [...prev, { message: msg, user }]);
+    socket.on('message:get', (msg) => {
+      const { chatMessage, room, user, error } = msg;
+      console.log('Обработал message:get...', msg, chatMessage);
+      setChatMessages((prev) => [...prev, chatMessage]);
     })
 
     return () => {
@@ -33,22 +35,20 @@ export default function Chat({ socket, stream_id = 1, user = 'Global' }) {
 
   const onClickHandler = () => {
     console.log('Отправка сообщения');
-    socket.emit('message:send', { message: inputValue, room: stream_id })
+    socket.emit('message:send', { message: inputValue, room: stream_id, user })
   }
 
   return (
     <div className='ChatList'>
-      <div className='chat-messages'>
-        {chatMessages.map((element) => (
-          <div key={element.id} className="containerMessage">
-            <img src="/w3images/bandmember.jpg" alt="Avatar" />
-            <p>{element.message}</p>
-            <span className="time-right">{Date.now().toLocaleString}</span>
-          </div>
-        ))}
-      </div>
+      {chatMessages.map((element) => (
+        <div key={element.id.toString()} className="containerMessage">
+          <p>{element.message}</p>
+          <span className="time-right">{Date.now().toLocaleString}</span>
+        </div>
+      ))}
       <input type="text" onChange={(e) => setInputValue(e.target.value)} value={inputValue} />
       <button type='button' onClick={onClickHandler}>Отправить</button>
     </div>
+
   )
 }
