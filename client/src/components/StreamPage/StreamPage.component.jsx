22c767140d72f03e2fe14cdc8e0579c05a92eefa
Artem <artemivlev@yandex.ru>
2022-03-20 10:19:17 +0300

    add stream state for StreamPage

--- client/src/components/StreamPage/StreamPage.component.jsx
+++ client/src/components/StreamPage/StreamPage.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom'
 import ReactPlayer from 'react-player/lazy';
 import styled from 'styled-components'
@@ -6,12 +6,23 @@ import {useSelector} from "react-redux";
 import {urlForStream} from '../../utils/fetchPath'
 
 const StreamPage = () => {
+	const [stream, setStream] = useState('')
 	const { streamId } = useParams();
-	const stream = useSelector(state => state.streams)
-	const currStream = stream.filter(el => el.broadcast_id === streamId)[0]
+	const currStream = useSelector(state => {
+					const streamRedux = state.streams;
+					return streamRedux.filter(el => el.broadcast_id === streamId)[0]
+	});
+
+	useEffect(() => {
+		if(currStream.length === 1) {
+			setStream(currStream[0].stream)
+		}
+	}, [currStream.length && currStream[0].stream]);
+
 	return (
 			<Wrapper>
-			<ReactPlayer url={ urlForStream(currStream.link)}/>
+				{/*<ReactPlayer  url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />*/}
+			<ReactPlayer  url={urlForStream(stream.source)} playing controls config={{file:{forceFLV:true}}}/>
 			</Wrapper>
 
 	)
@@ -20,5 +31,12 @@ const StreamPage = () => {
 export default StreamPage;
 
 const Wrapper = styled.div`
-	position: ;
+
 `
+
+
+//config={{
+// 				file:{
+// 					forceFLV: true,
+// 				}
+// 			}}
