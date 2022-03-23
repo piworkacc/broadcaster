d41f1f6767f6d9ba030fd438cb58a1adcbeb589b
Ekaterina <ekaterina.gnedenko@gmail.com>
2022-03-23 11:17:23 +0300

    Комменты: инпут нового коммента не показывается неавтовторизованным юзерам и ошибки больше не ломают фронт

--- client/src/components/ErrorComponent/index.js
+++ client/src/components/ErrorComponent/index.js
@@ -1,5 +1,5 @@
 function ErrorComponent({ error }) {
-  return <>{error && <div className="text-danger position-fixed">{`${error.name}: ${error.message}`}</div>}</>;
+  return <>{error && <div className="text-danger">{`${error.name}: ${error.message}`}</div>}</>;
 }
 
 export default ErrorComponent;
